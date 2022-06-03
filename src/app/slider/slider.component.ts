bb6b3777549f90b3df40846318033c4847508fca
TannerHartt <tannerhart24@yahoo.com>
2022-06-02 22:25:51 -0700

    Set up basic fade animation to the slides on the slider

--- src/app/slider/slider.component.ts
+++ src/app/slider/slider.component.ts
@@ -1,10 +1,17 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Movie } from "../../models/movie";
+import { animate, state, style, transition, trigger } from "@angular/animations";
 
 @Component({
   selector: 'app-slider',
   templateUrl: './slider.component.html',
-  styleUrls: ['./slider.component.css']
+  styleUrls: ['./slider.component.css'],
+  animations: [
+    trigger('fade', [
+      state('void', style({opacity: 0})),
+      transition('void <=> *', [animate('1s')])
+    ])
+  ]
 })
 export class SliderComponent implements OnInit {
 
