560b5ce8b8f45236c5bb4b3a4868168fab91611a
TannerHartt <tannerhart24@yahoo.com>
2022-06-02 19:41:27 -0700

    Created slider component to display the different movies with their respective details

---
+++ src/app/slider/slider.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Movie } from "../../models/movie";
+
+@Component({
+  selector: 'app-slider',
+  templateUrl: './slider.component.html',
+  styleUrls: ['./slider.component.css']
+})
+export class SliderComponent implements OnInit {
+
+  @Input() items: Movie[] = [];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
