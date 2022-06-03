2f8a438405588f8c7a05ac268843195962346aa3
TannerHartt <tannerhart24@yahoo.com>
2022-06-03 13:06:26 -0700

    Created component to display the list of items.

---
+++ src/app/items-banner/items-banner.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Movie } from "../../models/movie";
+
+@Component({
+  selector: 'app-items-banner',
+  templateUrl: './items-banner.component.html',
+  styleUrls: ['./items-banner.component.css']
+})
+export class ItemsBannerComponent implements OnInit {
+
+  @Input() items: Movie[] =[];
+  @Input() title: string = '';
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
