4f3cfa8cb7ffb2b59d33a1273d7c7b43025a7cc5
TannerHartt <tannerhart24@yahoo.com>
2022-06-03 18:08:32 -0700

    Added several service methods to grab more data from the api for use in the focused movie view.

--- src/app/services/movies.service.ts
+++ src/app/services/movies.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
-import { MovieDto } from "../../models/movie";
+import {Movie, MovieCredits, MovieDto, MovieImages, MovieVideoDto} from "../../models/movie";
 import { switchMap, of } from "rxjs";
 
 @Injectable({
@@ -13,7 +13,11 @@ export class MoviesService {
 
   constructor(private http: HttpClient) { }
 
+  getMovieDetails(id: string) {
+    return this.http.get<Movie>(`${this.baseUrl}/movie/${id}?api_key=${this.apiKey}`);
+  }
 
+  // Method used to get the movies in the "Home" tab.
   getMovies(type: string = '', count: number = 12) {
     return this.http.get<MovieDto>(`${this.baseUrl}/movie/${type}?api_key=${this.apiKey}`)
       .pipe(switchMap(res => {
@@ -22,12 +26,30 @@ export class MoviesService {
     );
   }
 
+  // Method used to get the movies in the "Movies" tab.
   searchMovies(page: number ) {
     return this.http.get<MovieDto>(`${this.baseUrl}/movie/popular?page=${page}&api_key=${this.apiKey}`)
       .pipe(switchMap(res => {
           return of(res.results);
         })
       );
+  }
+
+  getMovieVideos(id: string) {
+    return this.http.get<MovieVideoDto>(`${this.baseUrl}/movie/${id}/videos?api_key=${this.apiKey}`)
+      .pipe(
+        switchMap((res) => {
+          return of(res.results);
+        })
+      );
+  }
+
+  getMovieImages(id: string) {
+    return this.http.get<MovieImages>(`${this.baseUrl}/movie/${id}/images?api_key=${this.apiKey}`);
+  }
+
+  getMovieCredits(id: string) {
+    return this.http.get<MovieCredits>(`${this.baseUrl}/movie/${id}/credits?api_key=${this.apiKey}`);
   }
 
 }
