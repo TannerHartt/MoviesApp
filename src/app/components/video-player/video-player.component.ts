13f2b08c4f6f1e9ede21280a3390f6a2304dfd52
TannerHartt <tannerhart24@yahoo.com>
2022-06-03 18:02:17 -0700

    Created component to display a grid of embedded video players to display the video links from the api.

---
+++ src/app/components/video-player/video-player.component.ts
@@ -0,0 +1,32 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {DomSanitizer, SafeResourceUrl} from "@angular/platform-browser";
+
+@Component({
+  selector: 'app-video-player',
+  templateUrl: './video-player.component.html',
+  styleUrls: ['./video-player.component.css']
+})
+export class VideoPlayerComponent implements OnInit {
+
+  @Input() site: string = 'Youtube';
+  @Input() key: string | null = null;
+
+  videoUrl: SafeResourceUrl = '';
+
+  constructor(private sanitizer: DomSanitizer) { }
+
+  ngOnInit(): void {
+    switch (this.site) {
+      case 'Youtube':
+        this.videoUrl = this.getSafeUrl('https://www.youtube.com/embed/' + this.key);
+        break;
+      case 'Vimeo':
+        this.videoUrl = this.getSafeUrl('https://www.vimeo.com/embed' + this.key);
+        break;
+    }
+  }
+
+  getSafeUrl(url: string) {
+    return this.sanitizer.bypassSecurityTrustResourceUrl(url);
+  }
+}
