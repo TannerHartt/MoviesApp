0d380d337a06632bd35650a72332d05b17d9b33f
Tanner Hart <tannerhart24@yahoo.com>
2022-06-07 23:06:33 -0700

    Refactored to separate tv and movie service methods. Added several more tv methods

--- src/app/services/tv.service.ts
+++ src/app/services/tv.service.ts
@@ -1,9 +1,50 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { of, switchMap } from "rxjs";
+import {
+  TvImages,
+  TvShow,
+  TvShowDto
+} from "../../models/movie";
 
 @Injectable({
   providedIn: 'root'
 })
 export class TvService {
 
-  constructor() { }
+  baseUrl: string = "https://api.themoviedb.org/3";
+  apiKey: string = "7db05de00ae8c105292603bb77981acc";
+
+
+  constructor(private http: HttpClient) { }
+
+  getTvShows(page: number = 1) {
+    return this.http.get<TvShowDto>(`${this.baseUrl}/tv/popular?page=${page}&api_key=${this.apiKey}`)
+      .pipe(switchMap(res => {
+          return of(res.results);
+        })
+      );
+  }
+
+  getTvShowDetails(id: string) {
+    return this.http.get<TvShow>(`${this.baseUrl}/tv/${id}?api_key=${this.apiKey}`);
+  }
+  getSimilarShows(id: string) {
+    return this.http.get<TvShowDto>(`${this.baseUrl}/tv/${id}/similar?api_key=${this.apiKey}`)
+      .pipe(switchMap((res) => {
+          return of(res.results);
+        })
+      );
+  }
+  getTvShowImages(id: string) {
+    return this.http.get<TvImages>(`${this.baseUrl}/tv/${id}/images?api_key=${this.apiKey}`);
+  }
+
+  getShowRecommendations(id: string) {
+    return this.http.get<TvShowDto>(`${this.baseUrl}/tv/${id}/recommendations?api_key=${this.apiKey}`)
+      .pipe(switchMap(res => {
+        return of(res.results);
+      })
+    );
+  }
 }
